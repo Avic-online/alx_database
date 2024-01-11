80bbd27c710d2b51823cafff127d5ff7e8181a98
Avic-online <avicdon1@gmail.com>
2024-01-11 16:21:32 -0600

    taskes 7 to 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,7 @@
-
+-- code that list all cities in the database
+-- and store result in ascending order of their id 
+SELECT c.'id', c.'name', s.'name'
+    FROM 'cities' AS c
+        INNER JOIN 'states' AS s 
+        ON c.'state_id' = s.'id'
+    ORDER BY c.'id';
