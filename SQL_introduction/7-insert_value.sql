b96bb56faea717df59b1fad9479c36062daa25a7
Avic-online <avicdon1@gmail.com>
2024-02-03 16:02:11 +0100

    insertion done
    Signed-off-by: Avic-online <avicdon1@gmail.com>

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,7 +1,3 @@
 -- this code INSERTS TO A  table 
 USE hbtn_0c_0;
 INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
-
--- displays record of first table
-
-SELECT id, name FROM first_table ORDER BY id;
